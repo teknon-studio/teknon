27b0a73fe39008c6757490857102829920d48ecd
teknon-studio <thorneycroft.r@gmail.com>
2026-04-12 16:06:51 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { inject } from "@vercel/analytics";
-
-inject();
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
